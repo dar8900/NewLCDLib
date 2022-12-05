4917a72224a6c9d9c1f5d88d803d6e8c6e8b7fba
Dario Sadel VB <dar8900@gmail.com>
2022-12-05 08:58:51 +0100

    Modificato esempio

--- examples/newlcdlib_example/newlcdlib_example.ino
+++ examples/newlcdlib_example/newlcdlib_example.ino
@@ -5,7 +5,6 @@ NewLCDLib *lcd;
 void setup()
 {
     lcd = new NewLCDLib(0x27, NewLCDLib::LCD_16_2);
-    lcd->begin();
     lcd->clearScreen();
     lcd->printString(NewLCDLib::ONE, NewLCDLib::CENTER_ALIGN, "Hallo world!");
 }
